f2b4fca7d41cd8f8174b75acb1bd30826f4df028
Tomas Olalde <olaldetomas1@gmail.com>
2021-03-19 11:58:44 -0300

    arreglo mi pequeño error de concepto con las funciones estaticas

--- clase-practica-01/src/com/company/RadixSortEjerc.java
+++ clase-practica-01/src/com/company/RadixSortEjerc.java
@@ -7,32 +7,30 @@ import java.util.HashMap;
 public class RadixSortEjerc {
 
     public static void radixSort(Object[] arr) {
-        StringUtil stringUtil = new StringUtil();
         char x = '.';
         if(arr instanceof Integer[]) x = '0';
         if(arr instanceof String[]) x = 'a';
-        String[] normalizedArray = stringUtil.lNormalize(stringUtil.toStringArray(arr), x);
-        HashMap<String, ArrayList<String>> structure = stringUtil.createStructure(normalizedArray);
+
+        String[] normalizedArray = StringUtil.lNormalize(StringUtil.toStringArray(arr), x);
+        HashMap<String, ArrayList<String>> structure = StringUtil.createStructure(normalizedArray);
         int maxNumberLength = normalizedArray[0].length();
 
         for (int j = maxNumberLength - 1; j >= 0; j--) {
-            stringUtil.clearStructure(structure);
-            for (int i = 0; i < normalizedArray.length; i++) {
-                Character c = normalizedArray[i].charAt(j);
+            StringUtil.clearStructure(structure);
+            for (String s : normalizedArray) {
+                char c = s.charAt(j);
                 ArrayList<String> arrayList = structure.get(Character.toString(c));
-                arrayList.add(normalizedArray[i]);
+                arrayList.add(s);
             }
-            normalizedArray = stringUtil.rearmArray(structure);
+            normalizedArray = StringUtil.rearmArray(structure);
         }
 
         System.out.println(Arrays.toString(normalizedArray));
     }
 
     public static void main(String[] args) {
-        //Integer arr[] = {1623, 5, 898, 13, 906, 235, 1231, 1, 2, 3, 4, 23, 1532, 32, 2511, 8, 7, 0};
+        //Integer[] arr = {1623, 5, 898, 13, 906, 235, 1231, 1, 2, 3, 4, 23, 1532, 32, 2511, 8, 7, 0};
         String[] arr = {"asd", "ddd", "102931''¡3", "a", "das", "okp--", "aaa", "qwerty", "jeje", "jojo"};
-
         radixSort(arr);
     }
-
 }
